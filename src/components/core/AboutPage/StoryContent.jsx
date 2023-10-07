b777c3122fbd4a9a0672073de41cf35ad68ddef7
ram kumar <rammaniyari@gmail.com>
2023-10-07 21:14:22 +0530

    about page section 3 complete

---
+++ src/components/core/AboutPage/StoryContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StoryContent = ({text}) => {
+  return (
+    <p className='text-base font-medium text-richblack-300 lg:w-[95%]'>
+        {text}
+    </p>
+  )
+}
+
+export default StoryContent
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ import banner1 from "../assets/Images/aboutus1.webp";
 import banner2 from "../assets/Images/aboutus2.webp";
 import banner3 from "../assets/Images/aboutus3.webp";
 import Quote from "../components/core/AboutPage/Quote";
+import StoryContent from "../components/core/AboutPage/StoryContent";
+import foundingStory from "../assets/Images/FoundingStory.png";
 
 const About = () => {
   return (
@@ -36,13 +38,65 @@ const About = () => {
       <section className="border-b border-b-richblack-700">
         <div className="mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-richblack-500">
           <div className="h-[100px]"></div>
-            <Quote />
+          <Quote />
         </div>
       </section>
 
       {/* section 3  */}
 
+      <section>
+        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-richblack-500">
+          {/* first div  */}
+          <div className="flex flex-col items-center gap-10 lg:flex-row justify-between">
+            <div className="my-24 flex lg:w-[50%] flex-col gap-10">
+              <h2 className="bg-gradient-to-br from-[#833AB4] via-[#FD1D1D] to-[#FCB045] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%] ">
+                Our Founding Story{" "}
+              </h2>
+              <StoryContent
+                text={
+                  "Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world."
+                }
+              />
+              <StoryContent
+                text={
+                  "As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential."
+                }
+              />
+            </div>
+            <div>
+              <img
+                src={foundingStory}
+                alt=""
+                className="shadow-[0_0_20px_0] shadow-[#FC6767]"
+              />
+            </div>
+          </div>
 
+          {/* second div  */}
+          <div className="flex flex-col items-center lg:gap-10 lg:flex-row justify-between">
+            <div className="my-24 flex lg:w-[40%] flex-col gap-10">
+              <h2 className="bg-gradient-to-b from-[#FF512F] to-[#F09819] bg-clip-text text-4xl font-semibold text-transparent lg:w-[70%]">
+                Our Vision{" "}
+              </h2>
+              <StoryContent
+                text={
+                  "With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience."
+                }
+              />
+            </div>
+            <div className="my-24 flex lg:w-[40%] flex-col gap-10">
+              <h2 className="bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text text-4xl font-semibold lg:w-[70%] ">
+                Our Mission
+              </h2>
+              <StoryContent
+                text={
+                  "Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities."
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
